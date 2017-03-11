8544f05c659a6b1cd619a3fa5a771d3397b177ec
Oscar Bornemark <oscar.bornemark@gmail.com>
2017-03-12 00:17:39 +0100

    A lot of new data

--- data_creator.py
+++ data_creator.py
@@ -1,36 +1,73 @@
+# coding=utf-8
 from munch import Munch, munchify, unmunchify
 import json
 
 with open('rawdata/county_codes.json') as data_file:
-    datafile = json.load(data_file)
-    #print(datafile[2])
-    county_codes = Munch(datafile)
+    county_codes = json.load(data_file)
 
-print(county_codes)
+with open('rawdata/NV1701BO_reverse.json') as data_file:
+    kapacitetsutnyttjande = json.load(data_file)
 
-with open('rawdata/NV1701BO.json') as data_file:
-    kapacitetsutnyttjande = Munch(json.load(data_file))
+with open('rawdata/temp.json') as data_file:
+    temperatures = json.load(data_file)
+
+with open('rawdata/preci.json') as data_file:
+    preci = json.load(data_file)
+
+with open('rawdata/hotelnights.json') as data_file:
+    nights = json.load(data_file)
+
+
+
+def getOverallCoverForCounty(month, name):
+    county_list = kapacitetsutnyttjande["dataset"]["dimension"]["Region"]["category"]["index"]
+    i = county_list[name]
+    return kapacitetsutnyttjande["dataset"]["value"][month][i]
+
+def getTemperature(month, name):
+    return temperatures["temp"][month][name]
+
+def getPreci(month, name):
+    return preci[name][month]
+
+def getTotalNights(month, name):
+    county_list =  nights["dataset"]["dimension"]["Region"]["category"]["index"]
+    i = county_list[name]
+    return nights["dataset"]["value"][month][i]
+    #print(nights["dataset"]["value"][month][i])
+
+
+
+
 
 d = Munch()
 monthlist = []
-for x in xrange(0, 12):
+for month in range(0, 12):
     m = Munch(sweden={}, counties=[])
-    m.sweden = Munch(beds=123, cover=0.32, topCountries=[])
-    for xx in xrange(0, 3):
-        country = Munch(id="", name="", share=0.02)
+    m.sweden = Munch(beds="", nights="", cover="", topCountries=[])
+    m.sweden.nights = nights["dataset"]["value"][month][21]
+    for xx in range(0, 3):
+        country = Munch(id="", name="", share="")
         m.sweden.topCountries.append(country)
     m.counties = []
-    for y in xrange(0, 20):
-        county = Munch(id="", name="", weather={}, beds={}, cover={}, topCountries=[], foreginTourism={})
-
-        #county.name = county_codes[y].name
-        county.weather = Munch(temp=10, preci=25)
-        county.beds = Munch(all=7473, camp=31, hotel=984, hostel=234, cabin=37)
-        county.cover = Munch(all=0.7, camp=0.2, hotel=0.1, hostel=0.7, cabin=0.5, ofSweden=0.23)
-        for yy in xrange(0, 3):
-            country = Munch(id="", name="", share=0.02)
+    for y in range(0, 21):
+        countyName = county_codes[y]["name"]
+        county = Munch(id="", name="", weather={}, beds={}, nights={}, cover={}, topCountries=[], foreginTourism={})
+        county.id = county_codes[y]["id"]
+        county.name = countyName
+        county.weather = Munch()
+        county.weather.temp = getTemperature(month, countyName)
+        county.weather.preci = getPreci(month, countyName)
+        county.beds = Munch(all="", camp="", hotel="", hostel="", cabin="")
+        county.nights = Munch()
+        county.nights.all = getTotalNights(month, countyName)
+        county.nights.percentage = round(county.nights.all / m.sweden.nights * 100, 2)
+        county.cover = Munch(all="", camp="", hotel="", hostel="", cabin="", ofSweden="")
+        county.cover.all = getOverallCoverForCounty(month, countyName)
+        for yy in range(0, 3):
+            country = Munch(id="", name="", share="")
             county.topCountries.append(country)
-        county.foreginTourism = Munch(inCounty=0.3, inSweden=0.05)
+        county.foreginTourism = Munch(inCounty="", inSweden="")
         m.counties.append(county)
     monthlist.append(m)
 d.months = monthlist
